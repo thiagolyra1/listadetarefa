7e30c400c3d69f3cfe053e077318fa7ad9cf547e
Thiago Lyra <thiagolyra1@gmail.com>
2024-07-14 14:15:56 -0300

    ajuste no service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+
 import { Tarefa } from '../models/tarefa';
 import { Page } from '../models/page';
 
